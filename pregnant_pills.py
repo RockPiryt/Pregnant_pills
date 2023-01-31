f9b915b8ba44dfc6ee4d008efff77fea3fec1ca0
RockPiryt <rockpiryt@gmail.com>
2023-01-31 18:20:39 +0100

    Resolve problem with PDF_report function, use select option for choose pregnant week in add_user route, add delete button for one pill on list_pill page
    User can created PDF file report with informations about pills,
    which are on the user's list. User create PDF filename: 'pills_user_pk.pdf'
    after clicking on the blue button (pdf_report) on page with user list of pills.
    
    On the Add_user page user can now choose pregnant week from list.
    
    On the list_pill page user can delete pill. Now user should add id number of pill,
    which she want to remove. In future - user click delete button in pill row
    and the pill will be removed from database.

--- pregnant_pills.py
+++ pregnant_pills.py
@@ -42,10 +42,15 @@ class Pill(db.Model):
         self.user_id = user_id
 
     def __repr__(self):
-        if self.type_pill == 'special':
-            return f'Date: {self.week_start}, Pill name:{self.name}, amount: {self.amount}, (reason:{self.reason})'
-        else:
-            return f'Date: {self.week_start} - {self.week_end}, Pill name:{self.name}, amount: {self.amount}'
+            return f'Pill name:{self.name}'
+        
+
+
+    # def __repr__(self):
+    #     if self.type_pill == 'special':
+    #         return f'Date: {self.week_start}, Pill name:{self.name}, amount: {self.amount}, (reason:{self.reason})'
+    #     else:
+    #         return f'Date: {self.week_start} - {self.week_end}, Pill name:{self.name}, amount: {self.amount}'
 
 
 class User(db.Model):
@@ -67,40 +72,12 @@ class User(db.Model):
 
     def report_pills(self):
         for pill in self.pills:
-            return(pill)
+            return pill
 
 
 
 
 
-
-############PDF REPORTS#######################
-class PDF_report():
-
-    def __init__(self, filename):
-        self.filename = filename
-
-    def generate(self, user):
-        user_list_pills = user.report_pills()
-
-        pdf = FPDF(orientation='P', unit='pt', format='A4')
-        pdf.add_page()
-
-        #Title
-        pdf.set_font(family='Arial', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='User pills during pregnant', border=1, align='C', ln=1)
-
-        #User info
-        pdf.set_font(family='Arial', size=18, style='B')
-        pdf.cell(w=100, h=50, txt='Username:', border=1)
-        pdf.cell(w=100, h=50, txt= user.name, border=1, ln=1)
-
-        #Pills info
-        pdf.set_font(family='Arial', size=18)
-        pdf.cell(w=100, h=50, txt='Pills information:', border=1)
-        pdf.cell(w=200, h=200, txt= user_list_pills , border=1, ln=1)
-
-        pdf.output(self.filename)
 
 
 ############MAIN VIEWS###################
@@ -112,6 +89,8 @@ def index():
 ##################USER VIEWS####################
 @app.route('/add_user', methods=['GET','POST'])
 def add_user():
+    preg_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+    21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
 
     form = AddUserForm()
 
@@ -124,7 +103,7 @@ def add_user():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('user', user_primary_key=user.id))
-    return render_template('add_user.html', form=form)
+    return render_template('add_user.html', form=form, preg_list_whole = preg_list)
 
 
 @app.route('/<int:user_primary_key>/user/')
@@ -165,8 +144,9 @@ def add_pill(user_primary_key):
 def list_pill(user_primary_key):
     user = User.query.get_or_404(user_primary_key)
     pills = Pill.query.filter_by(user_id=user.id).all() # user_id to one to many powiązanie
-    # pill = Pill.query.get_or_404(pill_primary_key)
+    #one_pill = Pill.query.filter(db.and_(Pill.user_id==user.id, Pill.id==pills.id)).all()
     return render_template('list_pill.html', pills=pills, user=user)
+    #return render_template('list_pill.html', pills=pills, user=user, one_pill_p_key=one_pill.id)
 
 # @app.route('/del_pill', methods=['GET','POST'])
 # def del_pill_by_id():
@@ -180,33 +160,71 @@ def list_pill(user_primary_key):
 #         return redirect(url_for('list_pill'))
 #     return render_template('del_pill.html', form=form)
 
-@app.route('/del_pill', methods=['GET','POST'])
-def del_pill():
+@app.route('/<int:one_pill_p_key>/del_pill/', methods=['GET','POST'])
+def del_pill(one_pill_p_key):
     form = DelPillForm()
 
     if form.validate_on_submit():
-        id_pill = form.id_pill.data
-        pill = Pill.query.get(id_pill)
+        one_pill_p_key = form.id_pill.data
+        pill = Pill.query.get(one_pill_p_key)
         db.session.delete(pill)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('del_pill.html', form=form)
 
-# @app.post('/<int:pill_primary_key>/del_pill/') # post oznacza że routa przyjmuje tylko methode POST
-# def del_pill(pill_primary_key):
-#     pill = Pill.query.get_or_404(pill_primary_key)
+# @app.post('/<int:one_pill_p_key>/del_pill/') # post oznacza że routa przyjmuje tylko methode POST
+# def del_pill(one_pill_p_key):
+#     pill = Pill.query.get_or_404(one_pill_p_key)
 #     db.session.delete(pill)
 #     db.session.commit()
 #     return redirect(url_for('list_pill', user_primary_key=user.id))
 
 ##################PDF VIEWS####################
 
-@app.route('/pdf_create', methods=['GET','POST'])
-def pdf_create():
-    pdf_report = PDF_report(filename='pills.pdf')
-    user = User.query.filter_by(id=3).first()
-    pdf_report.generate(user)
-    return(f'PDF report was created')
+@app.route('/pdf_create/<int:user_primary_key>', methods=['GET','POST'])
+def pdf_create(user_primary_key):
+    pdf_report = PDF_report(filename='pills_user_pk.pdf')
+    user = User.query.get_or_404(user_primary_key)
+    pdf_report.generate_list(user)
+    return(f'PDF report was created. PDF filename: pills_user_pk.pdf')
+
+
+############PDF REPORTS#######################
+class PDF_report():
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate_list(self, user):
+        pills = Pill.query.filter_by(user_id=user.id).all() #tu słownik z tabletkami klasa
+        #muszę wyciągnąc name zeby był string bo tak to cała klase mi wyciąga
+        # pill = pills[2]
+        # for pill in pills:
+        #     return pill
+        # user_list_pills = user.report_pills()
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        #Title
+        pdf.set_font(family='Arial', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='User pills during pregnant', border=1, align='C', ln=1)
+
+        #User info
+        pdf.set_font(family='Arial', size=18, style='B')
+        pdf.cell(w=100, h=50, txt='Username:', border=1)
+        pdf.cell(w=100, h=50, txt= user.name, border=1, ln=1)
+
+        #Pills info
+        pdf.set_font(family='Arial', size=18)
+        pdf.cell(w=100, h=50, txt='Pills information:', border=1)
+        for pill in pills:
+            pdf.cell(w=200, h=200, txt= pill.name , border=1, ln=1)
+
+        pdf.output(self.filename)
+
+
+
 
 
 
