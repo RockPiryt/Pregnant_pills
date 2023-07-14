eafe39c8689a6c2f9fbc2724dca5bd2f8c844ef1
RockPiryt <rockpiryt@gmail.com>
2023-07-14 21:18:37 +0200

    'RockPiryt' - add a README file with previews and  description

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# Pregnant_pills
-App for storing infotmations about pills, which were taken during pregnant
+# Pregnant_Pills
+
+
+On this website you can create yourown list of pills,which you are taking during your pregnancy. It is very useful for your gynecologist and in case of hospital stay. You can specify if the pill is rutine or special.Rutine pills are vitamins and other complex nutrients, which you should take every day in pregnant. Special pills you use if you have illnessfor example flu, heartburn or gestational diabetes.You can download your pill's list as PDF Report.
+
+## Table of Contents
+
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Previews](#Previews)
+* [Setup](#setup)
+* [Project Status](#project-status)
+* [Room for Improvement](#room-for-improvement)
+* [Contact](#contact)
+* [License](#license)
+
+## General Information
+
+This web is for pregnant women to keep information about pills, which they taken during pregnancy. On home page User can register a new account. When user is logged in, she can create a list with pills. She can add a new pill or choose from database. She also add information about time (pregnancy week). After that user get all listed pills with dates and kind of pills. User can download this list as PDF file. 
+
+This application was created using Flask and SQLite database.
+
+## Technologies Used
+
+- Python - version 3.11
+- Bootstrap - version 5.2.3
+- Flask - version 2.2.2
+- SQLAlchemy - version 1.4.45
+- WTForms - version  3.0.1
+- SQLite database
+
+## Features
+
+List the ready features here:
+
+- Register new user,
+- Add pills to list,
+- Download list with pills as PDF file,
+
+## Previews
+
+### Home Page
+
+![Home Page Preview](static/previews/preview_pregnant_pills.jpg)
+
+### Register user page
+
+![Register user page](static/previews/preview_pregnant_pills_add_user.jpg)
+
+### Admin page - all users
+
+![Admin page - all users](static/previews/preview_pregnant_pills_users_list..jpg)
+
+## Setup
+
+- Clone This Project git clone
+- Enter Project Directory cd Pregnant_Pills
+- Create a Virtual Environment (for Windows) py -m venv (name your virtual enviroment :) venv
+
+'EXAMPLE: py -m venv venv'
+
+- Activate Virtual Environment source: venv/Scripts/activate
+- Install Requirements Package pip install -r requirements.txt
+- Finally Run The Project: python app.py
+
+## Project Status
+
+Project is: _in progress_
+
+## Room for Improvement
+
+Room for improvement:
+
+- Add PostgreSQL database,
+- Add feature -  register user,
+- Create more attractive PDF file,
+
+To do:
+- add a new Database_URI for PostgreSQL database and check models,
+- create functionality that only registered user can download a PDF file with list,
+- Add more information to PDF about user and about pills,
+
+## Contact
+
+- Created by [@RockPiryt Github](https://github.com/RockPiryt)
+- My Resume [@RockPiryt Resume](https://rockpiryt.github.io/Personal_Site/)
+
+Feel free to contact me!
+
+## License
+
+This project is open source and available under the [MIT License]
--- app.py
+++ app.py
@@ -8,19 +8,21 @@ from sqlalchemy.sql import func
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='4piers.teach.4Sows.3convex'
+app.config['SECRET_KEY'] = '4piers.teach.4Sows.3convex'
 app.app_context().push()
 
-###############DATABASE SQLITE#####################
+############### DATABASE SQLITE#####################
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///'+os.path.join(basedir, 'pill_db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'pill_db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
 
-#########################MODELS##########################
+######################### MODELS##########################
+
 
 class Pill(db.Model):
     __tablename__ = 'pills'
@@ -31,7 +33,7 @@ class Pill(db.Model):
     type_pill = db.Column(db.String(40))
     week_start = db.Column(db.Integer)
     week_end = db.Column(db.Integer)
-    add_date =db.Column(db.DateTime(timezone=True), server_default=func.now())
+    add_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
     reason = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -47,10 +49,7 @@ class Pill(db.Model):
         self.user_id = user_id
 
     def __repr__(self):
-            return f'Pill name:{self.name}'
-        
-
-
+        return f'Pill name:{self.name}'
 
     # def __repr__(self):
     #     if self.type_pill == 'special':
@@ -72,7 +71,7 @@ class User(db.Model):
         self.name = name
         self.surname = surname
         self.preg_week = preg_week
-    
+
     def __repr__(self):
         return f'Username: {self.name}, Surname: {self.surname}, Pregnant week: {self.preg_week}'
 
@@ -81,40 +80,35 @@ class User(db.Model):
             return pill
 
 
-
-
-
-
-
-############MAIN VIEWS###################
-
+############ MAIN VIEWS###################
 @app.route('/')
 def index():
     return render_template('home.html')
 
-##################USER VIEWS####################
-@app.route('/add_user', methods=['GET','POST'])
+################## USER VIEWS####################
+@app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
-    preg_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-    21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+    preg_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
 
     form = AddUserForm()
 
     if form.validate_on_submit():
         name = form.name.data
-        surname =form.surname.data
+        surname = form.surname.data
         preg_week = form.preg_week.data
-        user=User(name, surname, preg_week)
+        user = User(name, surname, preg_week)
 
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('user', user_primary_key=user.id))
-    return render_template('add_user.html', form=form, preg_list_whole = preg_list)
+    return render_template('add_user.html', form=form, preg_list_whole=preg_list)
 
 
 @app.route('/<int:user_primary_key>/user/')
 def user(user_primary_key):
-    user = User.query.get_or_404(user_primary_key) #to sortuje po primary key zawsze
+    # to sortuje po primary key zawsze
+    user = User.query.get_or_404(user_primary_key)
     return render_template('user.html', user=user, user_primary_key=user.id)
 
 
@@ -123,13 +117,12 @@ def all_users():
     users = User.query.all()
     return render_template('all_users.html', users=users)
 
-##################PILLS VIEWS####################
-
-@app.route('/add_pill/<int:user_primary_key>', methods=['GET','POST'])
+################## PILLS VIEWS####################
+@app.route('/add_pill/<int:user_primary_key>', methods=['GET', 'POST'])
 def add_pill(user_primary_key):
 
     form = AddPillForm()
-    user=User.query.get_or_404(user_primary_key)
+    user = User.query.get_or_404(user_primary_key)
 
     if form.validate_on_submit():
         name = form.name.data
@@ -142,21 +135,24 @@ def add_pill(user_primary_key):
         reason = form.reason.data
         user_id = user_primary_key
 
-        pill = Pill(name, choose_pill, amount, type_pill, week_start, week_end, add_date, reason, user_id)
+        pill = Pill(name, choose_pill, amount, type_pill,
+                    week_start, week_end, add_date, reason, user_id)
         db.session.add(pill)
         db.session.commit()
         flash(f'You add new pills {pill.name} to your medical diary!')
         return redirect(url_for('add_pill', user_primary_key=user.id))
         # return redirect(url_for('list_pill', user_primary_key=user.id))
-    return render_template('add_pill.html', form=form, user_id = user_primary_key, user=user)
+    return render_template('add_pill.html', form=form, user_id=user_primary_key, user=user)
+
 
 @app.route('/list_pill/<int:user_primary_key>')
 def list_pill(user_primary_key):
     user = User.query.get_or_404(user_primary_key)
-    pills = Pill.query.filter_by(user_id=user.id).all() # user_id to one to many powiązanie
-    #one_pill = Pill.query.filter(db.and_(Pill.user_id==user.id, Pill.id==pills.id)).all()
+    # user_id to one to many powiązanie
+    pills = Pill.query.filter_by(user_id=user.id).all()
+    # one_pill = Pill.query.filter(db.and_(Pill.user_id==user.id, Pill.id==pills.id)).all()
     return render_template('list_pill.html', pills=pills, user=user)
-    #return render_template('list_pill.html', pills=pills, user=user, one_pill_p_key=one_pill.id)
+    # return render_template('list_pill.html', pills=pills, user=user, one_pill_p_key=one_pill.id)
 
 # @app.route('/del_pill', methods=['GET','POST'])
 # def del_pill_by_id():
@@ -170,7 +166,8 @@ def list_pill(user_primary_key):
 #         return redirect(url_for('list_pill'))
 #     return render_template('del_pill.html', form=form)
 
-@app.route('/<int:one_pill_p_key>/del_pill/', methods=['GET','POST'])
+
+@app.route('/<int:one_pill_p_key>/del_pill/', methods=['GET', 'POST'])
 def del_pill(one_pill_p_key):
     form = DelPillForm()
 
@@ -189,25 +186,27 @@ def del_pill(one_pill_p_key):
 #     db.session.commit()
 #     return redirect(url_for('list_pill', user_primary_key=user.id))
 
-##################PDF VIEWS####################
+################## PDF VIEWS####################
 
-@app.route('/pdf_create/<int:user_primary_key>', methods=['GET','POST'])
+
+@app.route('/pdf_create/<int:user_primary_key>', methods=['GET', 'POST'])
 def pdf_create(user_primary_key):
     pdf_report = PDF_report(filename='pills_user_pk.pdf')
     user = User.query.get_or_404(user_primary_key)
     pdf_report.generate_list(user)
-    return(f'PDF report was created. PDF filename: pills_user_pk.pdf')
+    return (f'PDF report was created. PDF filename: pills_user_pk.pdf')
 
 
-############PDF REPORTS#######################
+############ PDF REPORTS#######################
 class PDF_report():
 
     def __init__(self, filename):
         self.filename = filename
 
     def generate_list(self, user):
-        pills = Pill.query.filter_by(user_id=user.id).all() #tu słownik z tabletkami klasa
-        #muszę wyciągnąc name zeby był string bo tak to cała klase mi wyciąga
+        # tu słownik z tabletkami klasa
+        pills = Pill.query.filter_by(user_id=user.id).all()
+        # muszę wyciągnąc name zeby był string bo tak to cała klase mi wyciąga
         # pill = pills[2]
         # for pill in pills:
         #     return pill
@@ -216,54 +215,52 @@ class PDF_report():
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
-        #Title
+        # Title
         pdf.set_font(family='Arial', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='User pills during pregnant', border=1, align='C', ln=1)
+        pdf.cell(w=0, h=80, txt='User pills during pregnant',
+                 border=1, align='C', ln=1)
 
-        #User info
+        # User info
         pdf.set_font(family='Arial', size=18, style='B')
         pdf.cell(w=100, h=50, txt='Username:', border=1)
-        pdf.cell(w=100, h=50, txt= user.name, border=1, ln=1)
+        pdf.cell(w=100, h=50, txt=user.name, border=1, ln=1)
 
-        #Pills info
+        # Pills info
         pdf.set_font(family='Arial', size=18)
         pdf.cell(w=100, h=50, txt='Pills information:', border=1)
         for pill in pills:
-            pdf.cell(w=200, h=200, txt= pill.name , border=1, ln=1)
+            pdf.cell(w=200, h=200, txt=pill.name, border=1, ln=1)
 
         pdf.output(self.filename)
 
 
-############ERRORS################
-#bad_request - brak jakiś danych w zapytaniu
+############ ERRORS################
+# bad_request - brak jakiś danych w zapytaniu
 @app.errorhandler(400)
 def bad_request(e):
     return render_template('400.html'), 400
 
-#unauthorized
+# unauthorized
 @app.errorhandler(401)
 def unauthorized(e):
     return render_template('401.html'), 401
 
-#page_not_found - brak rekordu w bd
+# page_not_found - brak rekordu w bd
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-#unsupported_media
+# unsupported_media
 @app.errorhandler(415)
 def unsupported_media(e):
     return render_template('415.html'), 415
 
-#internal_server_error
+# internal_server_error
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-
 ################################################
-
 if __name__ == '__main__':
     app.run(debug=True)
-
