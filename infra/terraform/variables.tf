0d72883e51f5d395dbf2845978ac3db4f1cc6040
RockPiryt <rockpiryt@gmail.com>
2025-10-26 12:47:50 +0100

    Add az and  sh_pub_key to variables

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -15,3 +15,14 @@ variable "region" {
   type        = string
   default     = "eu-west-1"
 }
+
+variable "az" {
+  description = "AWS Availability Zone"
+  type        = string
+  default     = "eu-west-1a"
+}
+
+variable "ssh_pub_key" {
+  description = "ssh public key"
+  type        = string
+}
