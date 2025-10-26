f42b60fe5558a86d4f0a85362bbc1649a7cd2d91
RockPiryt <rockpiryt@gmail.com>
2025-10-26 12:01:42 +0100

    Add gateway, route_table, route_table_associattion

--- infra/terraform/vm.tf
+++ infra/terraform/vm.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "debian" {
   most_recent      = true
-  owners           = ["136693071363"] id owner (amazon)z ami
+  owners           = ["136693071363"]
 
   filter {
     name   = "name"
-    values = ["debian-11-amd64-*"] skopiowana nazwa z ami
+    values = ["debian-11-amd64-*"]
   }
 
   filter {
@@ -13,20 +13,42 @@ data "aws_ami" "debian" {
   }
 }
 
-
-resource "aws_spot_instance_request" "preg_spot" {
-  ami           = data.aws_ami.debian.id
-  instance_type = "t3.micro"
-}
-
 resource "aws_vpc" "preg_vpc" {
   cidr_block       = "10.1.0.0/12"
   enable_dns_support  = true
   enable_dns_hostnames = true
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "main_preg" {
   vpc_id     = aws_vpc.preg_vpc.id
-  cidr_block = cidrsubnet(aws_vpc.preg_spot.cidr_block, 3, 1)
+  cidr_block = cidrsubnet(aws_vpc.preg_vpc.cidr_block, 3, 1)
   availability_zone =  var.region
+}
+
+resource "aws_internet_gateway" "gw_preg" {
+  vpc_id = aws_vpc.preg_vpc.id
+}
+
+resource "aws_route_table" "route_tb_preg" {
+  vpc_id = aws_vpc.preg_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_preg.id
+  }
+
+}
+
+resource "aws_route_table_association" "as_preg" {
+  subnet_id      = aws_subnet.main_preg.id
+  route_table_id = aws_route_table.route_tb_preg.id
+}
+
+resource "aws_spot_instance_request" "preg_spot" {
+  ami           = data.aws_ami.debian.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Preg-Spot"
+  }
 }
