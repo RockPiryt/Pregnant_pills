4b65da1bfd194ea1758def76dbdfd95245d1cd18
RockPiryt <rockpiryt@gmail.com>
2025-10-26 12:47:29 +0100

    Add security group, correct cidr_block, use correct az

--- infra/terraform/vm.tf
+++ infra/terraform/vm.tf
@@ -14,7 +14,7 @@ data "aws_ami" "debian" {
 }
 
 resource "aws_vpc" "preg_vpc" {
-  cidr_block       = "10.1.0.0/12"
+  cidr_block       = "10.0.0.0/16"
   enable_dns_support  = true
   enable_dns_hostnames = true
 }
@@ -22,7 +22,7 @@ resource "aws_vpc" "preg_vpc" {
 resource "aws_subnet" "main_preg" {
   vpc_id     = aws_vpc.preg_vpc.id
   cidr_block = cidrsubnet(aws_vpc.preg_vpc.cidr_block, 3, 1)
-  availability_zone =  var.region
+  availability_zone =  var.az
 }
 
 resource "aws_internet_gateway" "gw_preg" {
@@ -36,7 +36,6 @@ resource "aws_route_table" "route_tb_preg" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw_preg.id
   }
-
 }
 
 resource "aws_route_table_association" "as_preg" {
@@ -44,9 +43,45 @@ resource "aws_route_table_association" "as_preg" {
   route_table_id = aws_route_table.route_tb_preg.id
 }
 
+
+resource "aws_security_group" "ssh_preg" {
+  name = "allow-all"
+
+  vpc_id = aws_vpc.preg_vpc.id
+
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "preg_key_pair" {
+  key_name   = "preg-key"
+  public_key = file(var.ssh_pub_key)
+}
+
+
+
 resource "aws_spot_instance_request" "preg_spot" {
   ami           = data.aws_ami.debian.id
   instance_type = "t3.micro"
+  
+  key_name                    = resource.aws_key_pair.preg_key_pair.key_name
+  wait_for_fulfillment        = true
+  associate_public_ip_address = true
+  security_groups = ["${aws_security_group.ssh_preg.id}"]
+  subnet_id = aws_subnet.main_preg.id
 
   tags = {
     Name = "Preg-Spot"
