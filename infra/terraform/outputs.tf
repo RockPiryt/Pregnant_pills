c7f3d579e3b11e52406378e750dc0b12ad0a535e
RockPiryt <rockpiryt@gmail.com>
2025-11-07 17:43:49 +0100

    Add outputs with my public ip

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,5 +1,11 @@
 # wyciagniecie ip isntancji
 output "public_ip" {
+    description = "Publiczny adres IP instancji EC2"
     value = aws_spot_instance_request.preg_spot.public_ip
+}
+
+output "my_public_ip" {
+  description = "Publiczny adres IP, z którego mogę łączyć się przez SSH"
+  value = chomp(data.http.myip.response_body)
 }
 
