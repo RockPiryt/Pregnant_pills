50cf83e4018b4b51b7b086e25fe1d24f8b3cc38a
RockPiryt <rockpiryt@gmail.com>
2026-01-24 16:39:46 +0100

    Use script only to install k3 on ec2, deploy in ci/cd

---
+++ infra/terraform/preg-k3s/scripts/install_k3_deploy_app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive # installation does not prompt for input
+
+apt-get update -y
+apt-get install -y curl unzip
+
+# Instalacja K3s
+curl -sfL https://get.k3s.io | sh -
+
+until k3s kubectl get node &>/dev/null; do
+  echo "Czekam na uruchomienie K3s..."
+  sleep 5
+done
+
+mkdir -p /opt/k3s
+
+# Terraform wstrzyknie tu pliki YAML
+%{ for m in manifests ~}
+echo "Tworzę plik /opt/k3s/${m.name}"
+cat <<EOF > /opt/k3s/${m.name}
+${m.content}
+EOF
+%{ endfor ~}
+
+
+echo "Wdrażanie aplikacji..."
+for f in /opt/k3s/*.yaml; do
+  [ -e "$f" ] || continue
+  k3s kubectl apply -f "$f"
+done
+
+echo "✅ K3s i aplikacja wdrożone."
--- infra/terraform/preg-k3s/scripts/install_k3s.sh
+++ infra/terraform/preg-k3s/scripts/install_k3s.sh
@@ -5,29 +5,15 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install -y curl unzip
 
-# Instalacja K3s
+# Install K3s
 curl -sfL https://get.k3s.io | sh -
 
 until k3s kubectl get node &>/dev/null; do
-  echo "Czekam na uruchomienie K3s..."
+  echo "Wainting for K3s..."
   sleep 5
 done
 
-mkdir -p /opt/k3s
+# Allow non-root to read kubeconfig
+chmod 644 /etc/rancher/k3s/k3s.yaml
 
-# Terraform wstrzyknie tu pliki YAML
-%{ for m in manifests ~}
-echo "Tworzę plik /opt/k3s/${m.name}"
-cat <<EOF > /opt/k3s/${m.name}
-${m.content}
-EOF
-%{ endfor ~}
-
-
-echo "Wdrażanie aplikacji..."
-for f in /opt/k3s/*.yaml; do
-  [ -e "$f" ] || continue
-  k3s kubectl apply -f "$f"
-done
-
-echo "✅ K3s i aplikacja wdrożone."
+echo "K3s is ready."
