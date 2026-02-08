4ee8f0866e80eb6218770330a591f2a68ff1de15
RockPiryt <rockpiryt@gmail.com>
2026-02-08 18:09:27 +0100

    Allow ssh only form my IP, add ingress sg, add nodeport sg

--- infra/terraform/preg-k3s/core.tf
+++ infra/terraform/preg-k3s/core.tf
@@ -1,6 +1,4 @@
-# ===============================
 # CORE INFRA (bezpłatne zasoby)
-# ===============================
 
 # private cloud
 resource "aws_vpc" "preg_vpc" {
@@ -11,9 +9,10 @@ resource "aws_vpc" "preg_vpc" {
 
 # podział na 8 podsieci
 resource "aws_subnet" "main_preg" {
-  vpc_id     = aws_vpc.preg_vpc.id
-  cidr_block = cidrsubnet(aws_vpc.preg_vpc.cidr_block, 3, 1)
-  availability_zone =  var.az
+  vpc_id                  = aws_vpc.preg_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.preg_vpc.cidr_block, 3, 1)
+  availability_zone       = var.az
+  map_public_ip_on_launch = true
 }
 
 # utworzenie gateway (dostęp z zewnątrz)
@@ -43,47 +42,79 @@ data "http" "myip" {
 
 # sg dla ssh
 resource "aws_security_group" "ssh_preg" {
-  name = "allow-all"
-
-  vpc_id = aws_vpc.preg_vpc.id
+  name        = "preg-ssh"
+  description = "SSH tylko z mojego IP"
+  vpc_id      = aws_vpc.preg_vpc.id
 
   ingress {
-    # cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"] bezpieczne, ale zle dla runner
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    description = "SSH from my IP"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"] 
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "preg-ssh" }
+}
+
+
+# aplikacja przez NodePort: np. 30080
+resource "aws_security_group" "nodeport_preg" {
+  name   = "allow-nodeport"
+  vpc_id = aws_vpc.preg_vpc.id
+
+  ingress {
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# sg dla portu 80
-resource "aws_security_group" "http_preg" {
-  name = "allow-all-http"
-
-  vpc_id = aws_vpc.preg_vpc.id
+# ingress sg
+resource "aws_security_group" "ingress_preg" {
+  name        = "preg-ingress"
+  description = "Public HTTP/HTTPS dla ingress"
+  vpc_id      = aws_vpc.preg_vpc.id
 
   ingress {
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    description = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  ingress {
+    description = "HTTPS"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = { Name = "preg-ingress" }
 }
 
 # dodanie klucza do logowania, Klucz publiczny do SSH
