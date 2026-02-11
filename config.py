10dedfffd69eca7f4976295746ded87ec30ce1a9
RockPiryt <rockpiryt@gmail.com>
2026-02-11 15:13:28 +0100

    fix(app): use variable to set up environment

--- config.py
+++ config.py
@@ -1,45 +1,35 @@
 import os
-# from os.path import join, dirname
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
-# ---------Get secrets form .env file
-load_dotenv(find_dotenv())
+# Load .env only for local testing
+if os.getenv("APP_ENV") != "production":
+    load_dotenv()
 
-#----------Create Config class for app
 class Config(object):
-    SECRET_KEY = os.getenv("secret_key") or "secret_flask"
-    SQLALCHEMY_DATABASE_URI = ''  # DevelopmentConfig or TestingConfig add URI to database
+    SECRET_KEY = os.getenv("SECRET_KEY", "secret_flask")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#---------Configuration for Developing - host:localhost (PostgreSQL database on local server)
+# sqlite for development
 class DevelopmentConfig(Config):
-    # PostgreSQL database
-    username_postdb = os.getenv("postgreSQL_username")
-    password_postdb = os.getenv("postgreSQL_password")
-    database_postdb = os.getenv("postgreSQL_database")
-    host_postdb = os.getenv("postgreSQL_host")
-    SQLALCHEMY_DATABASE_URI= f"postgresql://{username_postdb}:{password_postdb}@{host_postdb}/{database_postdb}"
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(basedir, "pill_db_admin.sqlite")
+    )
     DEBUG = True
 
-# -------------------------Configuration for testing (SQLite database)
+# Postgres in Cluster
 class TestingConfig(Config):
-    # #SQLite database
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'pill_db_admin.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     DEBUG = True
     TESTING = True
 
-#---------Configuration for production (PostgreSQL database - AWS RDS)
+# AWS RDS
 class ProductionConfig(Config):
-    AWS_DB_HOST = os.getenv('DB_HOST')
-    AWS_DB_USERNAME = os.getenv('DB_USERNAME')
-    AWS_DB_PASSWORD = os.getenv('DB_PASSWORD')
-    AWS_DB_NAME = os.getenv('DB_NAME')
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{AWS_DB_USERNAME}:{AWS_DB_PASSWORD}@{AWS_DB_HOST}/{AWS_DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
-# ----------------------Config dict
 config_dict = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
 }
