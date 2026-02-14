8ba982671132ea6cdee844ee7682ba61013b0985
RockPiryt <rockpiryt@gmail.com>
2026-02-14 21:13:50 +0100

    chore(infra): Add EIP to output

--- infra/terraform/preg-k3s/outputs.tf
+++ infra/terraform/preg-k3s/outputs.tf
@@ -18,3 +18,9 @@ output "app_nodeport_url" {
   value       = "http://${aws_spot_instance_request.preg_spot.public_ip}:30080"
 }
 
+output "eip_public_ip" {
+  value       = try(aws_eip.preg_eip.public_ip, null)
+  description = "Elastic IP attached to the instance (if created)."
+}
+
+
