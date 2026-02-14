60d2259a671952daff19254e0390d0bb4ac75534
RockPiryt <rockpiryt@gmail.com>
2026-02-14 21:13:25 +0100

    chore(infra): Use subdomain for app

--- infra/terraform/preg-k3s/core.tf
+++ infra/terraform/preg-k3s/core.tf
@@ -123,6 +123,12 @@ resource "aws_key_pair" "preg_key_pair2" {
   public_key = file(var.ssh_pub_key)
 }
 
+# domena na Route 53
+data "aws_route53_zone" "pk_domain" {
+  name = "paulinakimak.com"
+  private_zone = false
+}
+
 # tworzenie stałego publicznego IP(bo spot może mieć zmienne IP)
 resource "aws_eip" "preg_eip" {
   tags = { Name = "preg-eip" }
@@ -132,10 +138,10 @@ resource "aws_eip_association" "preg_assoc" {
   instance_id   = aws_spot_instance_request.preg_spot.spot_instance_id
   allocation_id = aws_eip.preg_eip.id
 }
-#utworzenie rekordu dns dla EIP
-resource "aws_route53_record" "root" {
-  zone_id = aws_route53_zone.pk_domain.zone_id
-  name    = "paulinakimak.com"
+# utworzenie rekordu dns z subdomena dla EIP  (preg.paulinakimak.com)
+resource "aws_route53_record" "preg_app" {
+  zone_id = data.aws_route53_zone.pk_domain.zone_id
+  name    = "preg"
   type    = "A"
   ttl     = 300
   records = [aws_eip.preg_eip.public_ip]
