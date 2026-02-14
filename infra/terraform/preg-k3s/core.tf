5a4911711160e848f2ebe1d6d949e6922e9fddd5
RockPiryt <rockpiryt@gmail.com>
2026-02-14 10:45:59 +0100

    chore(infra): Create Elastic IP, associate with ec2 and create DNS record

--- infra/terraform/preg-k3s/core.tf
+++ infra/terraform/preg-k3s/core.tf
@@ -122,3 +122,22 @@ resource "aws_key_pair" "preg_key_pair2" {
   key_name   = "preg-key-2-pkimak"
   public_key = file(var.ssh_pub_key)
 }
+
+# tworzenie stałego publicznego IP(bo spot może mieć zmienne IP)
+resource "aws_eip" "preg_eip" {
+  vpc = true
+  tags = { Name = "preg-eip" }
+}
+# powiązanie EIP z instacja spot
+resource "aws_eip_association" "preg_assoc" {
+  instance_id   = aws_spot_instance_request.preg_spot.spot_instance_id
+  allocation_id = aws_eip.preg_eip.id
+}
+#utworzenie rekordu dns dla EIP
+resource "aws_route53_record" "root" {
+  zone_id = aws_route53_zone.pk_domain.zone_id
+  name    = "paulinakimak.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.preg_eip.public_ip]
+}
