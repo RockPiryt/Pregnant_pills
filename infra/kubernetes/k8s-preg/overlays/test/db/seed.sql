b0c9cda1b7242a1c72d2407cbdfcc7f10f065c8e
RockPiryt <rockpiryt@gmail.com>
2026-02-15 13:01:14 +0100

    chore(db): Update seed

--- infra/kubernetes/k8s-preg/overlays/test/db/seed.sql
+++ infra/kubernetes/k8s-preg/overlays/test/db/seed.sql
@@ -1,5 +1,24 @@
-INSERT INTO pills (name, type_pill, amount)
-VALUES
-  ('Aspirin', 'default', 1),
-  ('Ibuprofen', 'default', 1)
-ON CONFLICT DO NOTHING;
+-- 1) create seed user
+INSERT INTO users (name, surname, preg_week, email, password_hash)
+VALUES (
+  'Test',
+  'User',
+  12,
+  'seed@test.local',
+  'pbkdf2:sha256:600000$q0MQxlE3KJsUcJmI$d7440c232112473f9c5a96d9deb97e1255655ef0de45592fa0547b5c513f1b87'
+)
+ON CONFLICT (email) DO NOTHING;
+
+-- seed@test.local
+-- Test123!
+
+-- 2) pills for that user
+INSERT INTO pills (name, type_pill, amount, user_id)
+SELECT 'Aspirin', 'default', 1, id
+FROM users
+WHERE email = 'seed@test.local';
+
+INSERT INTO pills (name, type_pill, amount, user_id)
+SELECT 'Ibuprofen', 'default', 1, id
+FROM users
+WHERE email = 'seed@test.local';
