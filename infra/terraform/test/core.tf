c49e3b2617c97ca2bee2e78a5f78fff2083d9623
RockPiryt <rockpiryt@gmail.com>
2026-02-24 01:13:30 +0100

    chore(infra): Move dns releated resources to dns.tf

--- infra/terraform/test/core.tf
+++ infra/terraform/test/core.tf
@@ -102,12 +102,6 @@ resource "aws_key_pair" "preg_key_pair2" {
   public_key = file(var.ssh_pub_key)
 }
 
-# Route 53 domain
-data "aws_route53_zone" "pk_domain" {
-  name = "paulinakimak.com"
-  private_zone = false
-}
-
 # Elastic IP (fix IP for spot instance)
 resource "aws_eip" "preg_eip" {
   domain = "vpc"
@@ -118,11 +112,4 @@ resource "aws_eip_association" "preg_assoc" {
   instance_id   = aws_spot_instance_request.preg_spot.spot_instance_id
   allocation_id = aws_eip.preg_eip.id
 }
-# Route 53 record (subdomain preg.paulinakimak.com) for EIP
-resource "aws_route53_record" "preg_app" {
-  zone_id = data.aws_route53_zone.pk_domain.zone_id
-  name    = "preg"
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.preg_eip.public_ip]
-}
+
--- infra/terraform/test/dns.tf
+++ infra/terraform/test/dns.tf
@@ -1,19 +1,10 @@
-# domena na Route 53
+# Route 53 domain
 data "aws_route53_zone" "pk_domain" {
   name = "paulinakimak.com"
   private_zone = false
 }
 
-# tworzenie stałego publicznego IP(bo spot może mieć zmienne IP)
-resource "aws_eip" "preg_eip" {
-  tags = { Name = "preg-eip" }
-}
-# powiązanie EIP z instacja spot
-resource "aws_eip_association" "preg_assoc" {
-  instance_id   = aws_spot_instance_request.preg_spot.spot_instance_id
-  allocation_id = aws_eip.preg_eip.id
-}
-# utworzenie rekordu dns z subdomena dla EIP  (preg.paulinakimak.com)
+# Route 53 record (subdomain preg.paulinakimak.com) for EIP
 resource "aws_route53_record" "preg_app" {
   zone_id = data.aws_route53_zone.pk_domain.zone_id
   name    = "preg"
