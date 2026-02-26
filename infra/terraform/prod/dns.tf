f793729cbade6ff8a49d6e9285a5347197fdc2e8
Paulina Kimak <106155611+RockPiryt@users.noreply.github.com>
2026-02-26 16:47:20 +0100

    chore(dns): Translate comments and change order

--- infra/terraform/prod/dns.tf
+++ infra/terraform/prod/dns.tf
@@ -1,20 +1,21 @@
+# EIP (spot instance)
+resource "aws_eip" "preg_eip" {
+  tags = { Name = "preg-eip" }
+}
 
-# domena na Route 53
+# Associate EIP with intance
+resource "aws_eip_association" "preg_assoc" {
+  instance_id   = aws_spot_instance_request.preg_spot.spot_instance_id
+  allocation_id = aws_eip.preg_eip.id
+}
+
+# Route 53 domain
 data "aws_route53_zone" "pk_domain" {
   name = "paulinakimak.com"
   private_zone = false
 }
 
-# tworzenie stałego publicznego IP(bo spot może mieć zmienne IP)
-resource "aws_eip" "preg_eip" {
-  tags = { Name = "preg-eip" }
-}
-# powiązanie EIP z instacja spot
-resource "aws_eip_association" "preg_assoc" {
-  instance_id   = aws_spot_instance_request.preg_spot.spot_instance_id
-  allocation_id = aws_eip.preg_eip.id
-}
-# utworzenie rekordu dns z subdomena dla EIP  (preg.paulinakimak.com)
+# Route 53 record (subdomain preg.paulinakimak.com) for EIP
 resource "aws_route53_record" "preg_app" {
   zone_id = data.aws_route53_zone.pk_domain.zone_id
   name    = "preg"
@@ -22,3 +23,5 @@ resource "aws_route53_record" "preg_app" {
   ttl     = 300
   records = [aws_eip.preg_eip.public_ip]
 }
+
+
