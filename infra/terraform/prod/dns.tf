770877603deffef521f3995686bbcb83cd751976
Paulina Kimak <106155611+RockPiryt@users.noreply.github.com>
2026-02-26 16:51:08 +0100

    chore(dns): Create ACM cert for subdomain preg.paulinakimak.com

--- infra/terraform/prod/dns.tf
+++ infra/terraform/prod/dns.tf
@@ -25,3 +25,47 @@ resource "aws_route53_record" "preg_app" {
 }
 
 
+# AWS Certificate Manager Certificate for subdomain
+resource "aws_acm_certificate" "preg_aws_cert" {
+  domain_name       = "preg.paulinakimak.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "preg.paulinakimak.com"
+  }
+}
+
+# DNS validation records
+resource "aws_route53_record" "preg_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.preg_aws_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.pk_domain.zone_id
+}
+
+# Certificate validation
+resource "aws_acm_certificate_validation" "preg_cert" {
+  certificate_arn         = aws_acm_certificate.preg_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.preg_cert_validation : record.fqdn]
+}
+
+# Output ARN certyfikatu
+output "preg_certificate_arn" {
+  value = aws_acm_certificate_validation.preg_aws_cert.certificate_arn
+}
+
+
