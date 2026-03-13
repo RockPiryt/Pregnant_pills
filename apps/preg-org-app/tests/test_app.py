5c71bf4738f8c7f54d45066d5b3fc7f2c3c4245b
RockPiryt <rockpiryt@gmail.com>
2026-03-13 16:02:47 +0100

    fix(app): Remove unnecessary files form org app

--- apps/preg-org-app/tests/test_app.py
+++
@@ -1,5 +0,0 @@
-def test_index_page(client):
-    """Test index page loads correctly."""
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b'Pregnant' in response.data  
