b90a91a833682ee918c608375d48c8262fea8880
RockPiryt <rockpiryt@gmail.com>
2026-03-23 14:34:09 +0100

    fix(script): Install K3s worker with worker label already set

--- infra/terraform/prod/scripts/install_k3s_worker.sh
+++ infra/terraform/prod/scripts/install_k3s_worker.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-export DEBIAN_FRONTEND=noninteractive # no questions
+export DEBIAN_FRONTEND=noninteractive# no questions
 
 apt-get update -y
-apt-get install -y curl unzip
-apt-get install -y amazon-ssm-agent
+apt-get install -y curl unzip amazon-ssm-agent
 
 systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
 
-# Install K3s worker
+# Install K3s worker with worker label already set
 curl -sfL https://get.k3s.io | \
+  INSTALL_K3S_EXEC="agent --node-label node-role.kubernetes.io/worker=true" \
   K3S_URL="https://${MASTER_IP}:6443" \
   K3S_TOKEN="${K3S_TOKEN}" \
   sh -
-
-# Adding worker label
-NODE_NAME=$(hostname)
-until kubectl get node "$${NODE_NAME}" >/dev/null 2>&1; do
-  sleep 5
-done
-
-kubectl label node "$${NODE_NAME}" node-role.kubernetes.io/worker=true --overwrite
