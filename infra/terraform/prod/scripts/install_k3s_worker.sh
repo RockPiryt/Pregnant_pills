11288da6145c3766664c821e42fc48ed317b62d4
RockPiryt <rockpiryt@gmail.com>
2026-03-24 16:11:03 +0100

    fix(): Correct label to not use reserved label

--- infra/terraform/prod/scripts/install_k3s_worker.sh
+++ infra/terraform/prod/scripts/install_k3s_worker.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 exec > >(tee /var/log/install_k3s_worker.log | logger -t user-data -s 2>/dev/console) 2>&1
 
-export DEBIAN_FRONTEND=noninteractive# no questions
+export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
 apt-get install -y curl unzip ca-certificates
@@ -13,7 +13,7 @@ snap start amazon-ssm-agent || true
 
 # Install K3s worker with worker label already set
 curl -sfL https://get.k3s.io | \
-  INSTALL_K3S_EXEC="agent --node-label node-role.kubernetes.io/worker=true" \
+  INSTALL_K3S_EXEC="agent --node-label node-role=worker" \
   K3S_URL="https://${MASTER_IP}:6443" \
   K3S_TOKEN="${K3S_TOKEN}" \
   sh -
