e559f6068bcc28c1170022ea0127c91a34d06f6a
RockPiryt <rockpiryt@gmail.com>
2026-03-25 10:16:15 +0100

    fix(): Update comments and names in IAM resources and add policy

--- infra/terraform/prod/iam.tf
+++ infra/terraform/prod/iam.tf
@@ -1,4 +1,4 @@
-# Trust policy to allow EC2 to use role
+# Trust policy allowing EC2 instances to use this role
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     effect  = "Allow"
@@ -11,22 +11,22 @@ data "aws_iam_policy_document" "ec2_assume_role" {
   }
 }
 
-# Create ssm role for ec2
-resource "aws_iam_role" "ec2_ssm_role" {
-  name               = "preg-ec2-ssm-role"
+# IAM role shared by all EC2 instances in the K3s cluster
+resource "aws_iam_role" "ec2_node_role" {
+  name               = "preg-ec2-node-role"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
-# Add policy to ssm_role
+# Allow EC2 instances to connect through AWS Systems Manager
 resource "aws_iam_role_policy_attachment" "ec2_ssm_core" {
-  role       = aws_iam_role.ec2_ssm_role.name
+  role       = aws_iam_role.ec2_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# Add policy (EC2 has necessary Route53 permissions)
+# Allow ExternalDNS to manage Route53 DNS records
 resource "aws_iam_role_policy" "ec2_route53_policy" {
   name = "EC2Route53Policy"
-  role = aws_iam_role.ec2_ssm_role.id
+  role = aws_iam_role.ec2_node_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -45,9 +45,8 @@ resource "aws_iam_role_policy" "ec2_route53_policy" {
   })
 }
 
-
-# Add role to Instance Profile
-resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  name = "preg-ec2-ssm-profile"
-  role = aws_iam_role.ec2_ssm_role.name
+# Instance profile used by all EC2 nodes in the K3s cluster
+resource "aws_iam_instance_profile" "ec2_node_profile" {
+  name = "preg-ec2-node-profile"
+  role = aws_iam_role.ec2_node_role.name
 }
