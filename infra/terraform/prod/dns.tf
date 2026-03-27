7c91acf619a1e96f020eae4183e9f5735845367c
RockPiryt <rockpiryt@gmail.com>
2026-03-27 21:10:13 +0100

    fix(infra): Remove alb related resources

--- infra/terraform/prod/dns.tf
+++ infra/terraform/prod/dns.tf
@@ -9,50 +9,8 @@ resource "aws_route53_record" "preg_app" {
   zone_id = data.aws_route53_zone.pk_domain.zone_id
   name    = "preg"
   type    = "A"
-
-  alias {
-    name                   = aws_lb.preg_alb.dns_name
-    zone_id                = aws_lb.preg_alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
-# AWS Certificate Manager Certificate for subdomain
-resource "aws_acm_certificate" "preg_aws_cert" {
-  domain_name       = "preg.paulinakimak.com"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name = "preg.paulinakimak.com"
-  }
-}
-
-# DNS validation records
-resource "aws_route53_record" "preg_cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.preg_aws_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.pk_domain.zone_id
-}
-
-# Certificate validation
-resource "aws_acm_certificate_validation" "preg_cert_validation" {
-  certificate_arn         = aws_acm_certificate.preg_aws_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.preg_cert_validation : record.fqdn]
+  ttl     = 300
+  records = [aws_eip.preg_worker_a_eip.public_ip]
 }
 
 
