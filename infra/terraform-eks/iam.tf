63d881ed4f52f921ef5efecacb6da7b9b5bc60f2
RockPiryt <rockpiryt@gmail.com>
2026-03-30 13:42:34 +0200

    feat(): Add required IAM roles

--- infra/terraform-eks/iam.tf
+++ infra/terraform-eks/iam.tf
@@ -1,3 +1,28 @@
+# Trust policy allowing EKS to assume the cluster role.
+data "aws_iam_policy_document" "eks_cluster_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+# IAM role used by the EKS control plane.
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "${var.cluster_name}-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role.json
+}
+
+# Allows the EKS control plane to manage the cluster.
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
 # Trust policy allowing EC2 instances to assume the node role.
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
@@ -39,4 +64,34 @@ resource "aws_iam_role_policy_attachment" "ecr_readonly" {
 resource "aws_iam_role_policy_attachment" "ssm_core" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Allows worker nodes to interact with Auto Scaling groups.
+resource "aws_iam_role_policy_attachment" "asg_full_access" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
+}
+
+# Allows worker nodes to manage Route53 hosted zones and records.
+resource "aws_iam_role_policy_attachment" "route53_full_access" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+# Allows worker nodes full access to Amazon ECR.
+resource "aws_iam_role_policy_attachment" "ecr_full_access" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+# Allows worker nodes to use AWS App Mesh.
+resource "aws_iam_role_policy_attachment" "appmesh_full_access" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSAppMeshFullAccess"
+}
+
+# Allows worker nodes to manage Elastic Load Balancers.
+resource "aws_iam_role_policy_attachment" "elb_full_access" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
 }
